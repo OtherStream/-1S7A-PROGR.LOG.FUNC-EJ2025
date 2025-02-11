01bb633c3889b0e28349c361da0bf112ca71f703
OtherStream <143450411+OtherStream@users.noreply.github.com>
2025-02-10 22:35:34 -0600

    Update Ejercicio: Kata FizzBuzz.hs

--- Ejercicio: Kata FizzBuzz.hs
+++ Ejercicio: Kata FizzBuzz.hs
@@ -1,50 +1,43 @@
-module FizzBuzz where
+ifEntoncesSiNo :: Bool -> a -> a -> a
+ifEntoncesSiNo condicion valorSi valorNo =
+    case condicion of
+        True -> valorSi
+        False -> valorNo    
 
-ifThenElse :: Bool -> a -> a -> a
-ifThenElse cond thenVal elseVal =
-    case cond of
-        True -> thenVal
-        False -> elseVal    
-
--- Función para la lógica de FizzBuzz
 fizzbuzz :: Int -> String
 fizzbuzz n
     | n `mod` 15 == 0 = "FizzBuzz!"
     | n `mod` 3 == 0  = "Fizz!"
     | n `mod` 5 == 0  = "Buzz!"
-    | otherwise       = number n
+    | otherwise       = numeroEnPalabras n
 
--- Números menores a 20 en palabras
-lessThan20 :: Int -> String           
-lessThan20 n
+menorQue20 :: Int -> String           
+menorQue20 n
     | n > 0 && n < 20 = 
-        let answers = words ("one two three four five six seven eight nine ten " ++
-                             "eleven twelve thirteen fourteen fifteen sixteen " ++
-                             "seventeen eighteen nineteen")
-        in answers !! (n - 1) 
+        let respuestas = words ("one two three four five six seven eight nine ten " ++
+                                "eleven twelve thirteen fourteen fifteen sixteen " ++
+                                "seventeen eighteen nineteen")
+        in respuestas !! (n - 1) 
     | otherwise = ""
 
--- Decenas en palabras
-tens :: Int -> String
-tens n 
+decenas :: Int -> String
+decenas n 
     | n >= 2 && n <= 9 =
-        let answers = words "twenty thirty forty fifty sixty seventy eighty ninety"
-        in answers !! (n - 2)
+        let respuestas = words "twenty thirty forty fifty sixty seventy eighty ninety"
+        in respuestas !! (n - 2)
     | otherwise = ""
 
--- Convertir número a palabras
-number :: Int -> String
-number n 
-    | n > 0 && n < 20 = lessThan20 n
-    | n `mod` 10 == 0 && n < 100  = tens (n `div` 10)
-    | n < 100 = tens (n `div` 10) ++ "-" ++ lessThan20 (n `mod` 10)
+numeroEnPalabras :: Int -> String
+numeroEnPalabras n 
+    | n > 0 && n < 20 = menorQue20 n
+    | n `mod` 10 == 0 && n < 100  = decenas (n `div` 10)
+    | n < 100 = decenas (n `div` 10) ++ "-" ++ menorQue20 (n `mod` 10)
     | n == 100 = "one hundred"
     | otherwise = show n
 
--- Función principal para ejecutar el programa
 main :: IO ()
 main = do
     putStrLn "Ingrese un número límite:"
-    input <- getLine
-    let num = read input :: Int
-    mapM_ (putStrLn . fizzbuzz) [1..num]
+    entrada <- getLine
+    let numero = read entrada :: Int
+    mapM_ (putStrLn . fizzbuzz) [1..numero]
